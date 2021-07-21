6555636fa8aed4fd9dbebf9e727f4ef8042d674e
Rajan Gautam <gautamrajan073@gmail.com>
2021-07-21 23:38:49 +0530

    Made it Better

--- theblog/forms.py
+++ theblog/forms.py
@@ -1,16 +1,21 @@
 from django import forms
 from .models import Post, Profile, Comment, Category
 
-# choices = [('Food', 'Food'), ('Travel', 'Travel')]
-choices = Category.objects.all().values_list('name', 'name')
-
 choice_list = []
-
+choices = Category.objects.all().values_list('name', 'name')
 for item in choices:
     choice_list.append(item)
 
 
 class PostForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        choices = Category.objects.all().values_list('name', 'name')
+        choice_list = []
+
+        for item in choices:
+            choice_list.append(item)
+        self.fields['category'].choices = choice_list
 
     class Meta:
         model = Post
