6cb1d3a19bd8145a3082cd0f3a69015c9c44c41e
Rajan Gautam <gautamrajan073@gmail.com>
2021-11-22 14:25:03 +0530

    Fixed Error

--- ablog/settings.py
+++ ablog/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = config['configuration']['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['web-tech-lab-43.herokuapp.com']
+ALLOWED_HOSTS = ['rgautam320-blog.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
